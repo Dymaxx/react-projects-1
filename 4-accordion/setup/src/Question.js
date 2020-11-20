72db8314b7065688475f9ee91646e035349800cf
Dymaxx <remcosmurf1@gmail.com>
2020-11-20 17:37:37 +0100

    first commit

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import questions from './data';
+const Question = ({question}) => {
+  const [showAnswer,setShowAnswer] = useState(false)
+  const {id,title,info} = question
+
+
+  
+  return <article className='question'>
+    <header>
+      <h4>{title}</h4>
+<button className='btn' onClick={() => setShowAnswer(!showAnswer)}>{!showAnswer ? <AiOutlinePlus/>:<AiOutlineMinus/>}</button>
+    </header>
+
+{!showAnswer || <p className='correct-answers'>{info}</p>}
+    </article>;
 };
 
 export default Question;
--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,8 +3,33 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCatagories = ['all', ...new Set(items.map((items) => items.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCatagories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((items) => items.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={allCatagories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
